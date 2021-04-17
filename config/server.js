456760134b79f2d712d250e868ece98147106691
Pedro <pedro-gomes17@hotmail.com>
2021-04-17 14:20:39 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5363ead36392455eb98f4754aed325c2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ba76b44f-f525-4ad5-a194-f324ff889dc1'
+};
